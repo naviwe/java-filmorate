0211d67fcd22fbc46104b749b92dcc152b7937c2
naviwe <timurnovotorzhin@yandex.kz>
2024-12-28 13:43:19 +0400

    fix: code style

--- src/main/java/ru/yandex/practicum/filmorate/model/film/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/film/Film.java
@@ -5,14 +5,18 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.LinkedHashSet;
 
 @Builder
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Film {
     private Long id;
 
@@ -34,4 +38,5 @@ public class Film {
 
     private LinkedHashSet<Genre> genres;
 }
+
 
